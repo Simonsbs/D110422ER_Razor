58ae2d9ad812b0cad4d4de989267959ccfa9b2e1
Simon B.Stirling <simonsbs@gmail.com>
2023-05-15 18:34:29 +0300

    Added routes and call to RouteData

--- MyShop/MyShop/Pages/Products/AddProduct.cshtml.cs
+++ MyShop/MyShop/Pages/Products/AddProduct.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyShop.Pages
+namespace MyShop.Pages.Products
 {
-    public class NewPageModel : PageModel
+    public class IndexModel : PageModel
     {
         public void OnGet()
         {
