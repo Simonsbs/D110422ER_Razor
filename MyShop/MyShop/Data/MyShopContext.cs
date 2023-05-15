466987ef89b510a3a8a9b1c5610a75d3afeeee62
Simon B.Stirling <simonsbs@gmail.com>
2023-05-15 20:36:56 +0300

    Added db seed

--- MyShop/MyShop/Data/MyShopContext.cs
+++ MyShop/MyShop/Data/MyShopContext.cs
@@ -7,5 +7,24 @@ namespace MyShop.Data {
         }
 
         DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Product>().HasData(
+                new Product {
+                    ID = 1,
+                    Name = "Product 1",
+                    Description = "Description 1",
+                    Price = 10.0,
+                    Category = "Cat1"
+                },
+                new Product {
+                    ID = 2,
+                    Name = "Product 2",
+                    Description = "Description 2",
+                    Price = 20.0,
+                    Category = "Cat2"
+                }
+            );
+        }
     }
 }
