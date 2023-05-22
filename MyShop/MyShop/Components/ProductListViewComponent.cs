dd01a063371123faafb7c5f16172030093cb5be3
Simon B.Stirling <simonsbs@gmail.com>
2023-05-22 19:51:14 +0300

    Created and implemented ProductList component

---
+++ MyShop/MyShop/Components/ProductListViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Data;
+using MyShop.Data.Models;
+
+namespace MyShop.Components {
+    public class ProductListViewComponent : ViewComponent {
+        private readonly IProductRepository _repo;
+
+        public ProductListViewComponent(IProductRepository repo) {
+            _repo = repo ?? throw new ArgumentNullException(nameof(repo));
+        }
+
+        public IViewComponentResult Invoke() {
+            List<Product> items = _repo.GetAll();
+            return View(items);
+        }
+    }
+}
